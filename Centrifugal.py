47977b816845d19e6b5d1d4f522f2cd66ac4d1d4
safasfeh <aalsafasfeh@ttu.edu.jo>
2025-05-18 21:10:16 +0300

    Centrifugal.py

--- Centrifugal.py
+++ Centrifugal.py
@@ -6,11 +6,11 @@ from io import BytesIO
 
 st.set_page_config(page_title="Centrifugal Separation Simulator", layout="wide")
 # Centered TTU Logo and Header Info
-st.image("ttu_logo.png", width=800)
+st.image("ttu_logo.png", width=400)
 st.markdown("""
     <div style='text-align: center;'>
-        <h3 style='color: green;'>Tafila Technical University</h3>
-        <h4 style='color: green;'>Natural Resources and Chemical Engineering Department</h4>
+        <h3 style='color: blue;'>Tafila Technical University</h3>
+        <h4 style='color: blue;'>Natural Resources and Chemical Engineering Department</h4>
         <p><strong>Bachelor's Degree Project</strong></p>
     </div>
 """, unsafe_allow_html=True)
@@ -18,13 +18,14 @@ st.markdown("""
 # Project Info Box
 st.markdown("""
     <div style='border: 2px solid #ddd; padding: 15px; border-radius: 10px; margin-top: 10px;'>
-        <h2 style='text-align: center;'>Modeling Coagulation–Flocculation with Artificial Neural Networks</h2>
-        <h4 style='text-align: center;'>Operation Parameters Prediction</h4>
+        <h2 style='text-align: center;'>Centrifugal Separation Device Simulator</h2>
+        
         <br>
         <p><strong>Students:</strong><br>
-        Shahad Mohammed Abushamma<br>
-        Rahaf Ramzi Al -shakh Qasem<br>
-        Duaa Musa Al-Khalafat</p>
+        Ahmad Al-Khalayleh<br>
+        Mohammad Al-Khalayleh<br>
+        Hebatullh Abuabboud<br>
+        Doaa Al-Shoha</p>
         <p><strong>Supervisor:</strong> Dr. Ashraf Alsafasfeh</p>
     </div>
 """, unsafe_allow_html=True)
